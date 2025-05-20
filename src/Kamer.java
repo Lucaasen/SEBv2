ee593e2550b0b509a0704db956203d103dfdd569
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-05-20 20:04:43 +0200

    Worked on Kamer class

--- src/Kamer.java
+++ src/Kamer.java
@@ -1,3 +1,36 @@
 public interface Kamer {
     void betreed(Speler speler);
 }
+
+public abstract class Kamer {
+    protected String naam;
+    protected String omschrijving;
+
+    public Kamer(String naam, String omschrijving) {
+        this.naam = naam;
+        this.omschrijving = omschrijving;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public abstract void betreed(Speler speler);
+}
+
+public class SprintPlanningKamer extends Kamer {
+    public SprintPlanningKamer() {
+        super("Sprint Planning", "Je moet inschatten welke taken passen binnen een sprint.");
+    }
+
+    @Override
+    public void betreed(Speler speler) {
+        System.out.println("Welkom in de kamer: " + naam);
+        System.out.println(omschrijving);
+    }
+}
+
