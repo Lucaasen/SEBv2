22c455061072a64e4ccb2388883be48138cb6aa2
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-05-23 13:31:18 +0200

    Added subclasses to Kamer

--- src/Kamer.java
+++ src/Kamer.java
@@ -40,3 +40,32 @@ class SprintPlanningKamer extends Kamer {
         System.out.println("Opdracht: Kies de juiste taken voor de sprint!");
     }
 }
+
+class DailyScrumKamer extends Kamer {
+
+}
+
+class ScrumBoardKamer extends Kamer {
+
+}
+
+class SprintReviewKamer extends Kamer {
+
+}
+
+class RetrospectiveKamer extends Kamer {
+    public RetrospectiveKamer() {
+        super("Retrospective", "Reflecteer op de afgelopen sprint.");
+    }
+
+    @Override
+    public void betreed(Speler speler) {
+        beschrijving();
+        voerOpdrachtUit(speler);
+    }
+
+    @Override
+    public void voerOpdrachtUit(Speler speler) {
+        System.out.println("Opdracht: Wat ging goed en wat kan beter?");
+    }
+}
