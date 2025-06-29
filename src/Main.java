3f501683850a933116da5ca6dd11e1233d001f5c
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-06-29 19:03:03 +0200

    Added methjod which starts the exam system

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//het programma starten
-
 public class Main {
     public static void main(String[] args) {
-
-        //maak een nieuw Game-object
         Game game = new Game();
-        game.addObserver(new GameLogger());
-        //start het spel
+        game.addObserver(new GameLogger()); 
+        ExamObserver observer = new ExamObserver();
+        game.addObserver(observer); 
+
         game.start();
-        // Log het moment dat de speler het spel start
+
+        ExamSystem exam = new ExamSystem(observer);
+        exam.start();
+        
     }
 }
